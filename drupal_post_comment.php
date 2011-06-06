30c69ee38c7f4b84adb371dd769fdc6f98e6d7d7
Liraz Siri <liraz@sterilesecurity.com>
2011-06-06 10:52:23 +0300

    hacked drupal_post_command to work with posts by non-logged in users

--- drupal_post_comment.php
+++ drupal_post_comment.php
@@ -7,17 +7,21 @@ function mail2uid($mail) {
     return db_result(db_query("SELECT uid FROM {users} WHERE LOWER(mail) = LOWER('%s')", $mail));
 }
 
+function get_sender_address($mail) {
+    if(preg_match('/<(.*)>/', $mail, $match)) {
+        return $match[1];
+    }
+    return $mail;
+}
 
 function post_comment($path, $user_mail, $subject, $comment) {
+    global $base_url;
 	chdir("/usr/share/drupal6");
 
 	require_once './includes/bootstrap.inc';
 	drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
-    $uid = mail2uid($user_mail);
-    if(!$uid)
-		throw new Exception("no such user {$user_mail}");
-
+    $uid = mail2uid(get_sender_address($user_mail));
 	$pid = NULL;
 	if(preg_match('/(.*)#comment-(\d+)/', $path, $m)) {
 		$path = $m[1];
@@ -30,12 +34,24 @@ function post_comment($path, $user_mail, $subject, $comment) {
 	
 	$nid = $m[1];
 	$comment = array(
-		'uid' => $uid,
+		'uid' => $uid ? $uid : 0,
 		'nid' => $nid,
 		'pid' => $pid,
 		'subject' => $subject,
 		'comment' => $comment
 	);
+    if($uid) {
+        $user = user_load($uid);
+        $comment['name'] = $user->name;
+        $comment['mail'] = $user->mail;
+    } else {
+        if(preg_match('/^(.*?)\s*<(.*)>/', $user_mail, $match)) {
+            $comment['name'] = $match[1];
+            $comment['mail'] = $match[2];
+        } else {
+            $comment['mail'] = $user_mail;
+        }
+    }
 
     _comment_form_submit($comment); // e.g., fix empty subject
 	comment_save($comment);
@@ -44,19 +60,19 @@ function post_comment($path, $user_mail, $subject, $comment) {
 function main($args) {
 
 	if(count($args) != 3) {
-		print "Syntax: {$args[0]} path[#comment-\$cid] user-mail\n\n";
+		print "Syntax: {$args[0]} path[#comment-\$cid] urlencoded(user-mail)\n\n";
 		print "Example usage:\n\n";
 		print "    (echo 'comment title'; cat comment_body.txt) | {$args[0]} blog/great-post#comment-1234 2\n";
 		exit(1);
 	}
 
 	$url = $args[1];
-    $user_mail = $args[2];
+    $user_mail = urldecode($args[2]);
 
 	/* get comment from stdin */
 	$fh = fopen("php://stdin", "r");
 
-	$subject = fgets($fh);
+	$subject = rtrim(fgets($fh));
 	$comment = stream_get_contents($fh);
 
 	post_comment($url, $user_mail, $subject, $comment);
