6df6558e9c423d56a7c778aefb7b8f2b9d2de76c
Liraz Siri <liraz@sterilesecurity.com>
2011-06-08 23:28:40 +0300

    refactored disconnected functions into Context class

--- cmd_debug.py
+++ cmd_debug.py
@@ -26,48 +26,86 @@ def makedirs(path):
         if e.errno != errno.EEXIST:
             raise
 
-class DebugPaths:
-    SUBDIRS = ['command', 'id', 'env', 'stdin', 'stdout', 'stderr', 'exitcode', 'rerun']
+
+class Context:
+    class ContextPaths:
+        SUBDIRS = ['command', 'id', 'env', 'stdin', 'stdout', 'stderr', 'exitcode', 'rerun']
+        def __init__(self, path):
+            self.path = path
+            for subdir in self.SUBDIRS:
+                setattr(self, subdir, join(path, subdir))
+
     def __init__(self, path):
         self.path = path
-        for subdir in self.SUBDIRS:
-            setattr(self, subdir, join(path, subdir))
+        self.subpath = self.ContextPaths(path)
 
-def fmt_command(argv):
-    if not argv:
-        return ""
+    @staticmethod
+    def fmt_command(argv):
+        if not argv:
+            return ""
 
-    args = argv[1:]
+        args = argv[1:]
 
-    for i, arg in enumerate(args):
-        if re.search(r"[\s'\"]", arg):
-            args[i] = commands.mkarg(arg)
-        else:
-            args[i] = " " + arg
+        for i, arg in enumerate(args):
+            if re.search(r"[\s'\"]", arg):
+                args[i] = commands.mkarg(arg)
+            else:
+                args[i] = " " + arg
 
-    return argv[0] + "".join(args)
+        return argv[0] + "".join(args)
 
-def fmt_id():
-    id = "%d:%d:%s" % (os.getuid(), os.getgid(), 
-                       ",".join([ str(group) for group in os.getgroups() ]))
-    return id
+    @staticmethod
+    def fmt_id():
+        id = "%d:%d:%s" % (os.getuid(), os.getgid(), 
+                           ",".join([ str(group) for group in os.getgroups() ]))
+        return id
 
-def fmt_env():
-    sio = StringIO()
-    for var, val in os.environ.items():
-        print >> sio, "%s=%s" % (var, val)
+    @staticmethod
+    def fmt_env():
+        sio = StringIO()
+        for var, val in os.environ.items():
+            print >> sio, "%s=%s" % (var, val)
 
-    return sio.getvalue()
+        return sio.getvalue()
 
-def run(command, input=None):
-    try:
-        child = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        stdout, stderr = child.communicate(input)
+    @staticmethod
+    def run(command, input=None):
+        try:
+            child = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            stdout, stderr = child.communicate(input)
 
-        return stdout, stderr, child.returncode
+            return stdout, stderr, child.returncode
 
-    except OSError, e:
-        return "", str(e), None
+        except OSError, e:
+            return "", str(e), None
+
+    def save(self, input=None, command=None):
+        makedirs(self.path)
+        subpath = self.subpath
+
+        print >> file(subpath.id, "w"), self.fmt_id()
+        print >> file(subpath.env, "w").write(self.fmt_env())
+
+        if input:
+            file(subpath.stdin, "w").write(input)
+
+        try:
+            os.symlink(sys.argv[0], subpath.rerun)
+        except OSError:
+            pass
+
+        if command:
+            print >> file(subpath.command, "w"), self.fmt_command(command)
+
+            stdout, stderr, exitcode = run(command, input)
+            if stdout:
+                file(subpath.stdout, "w").write(stdout)
+
+            if stderr:
+                file(subpath.stderr, "w").write(stderr)
+
+            if exitcode is not None:
+                print >> file(subpath.exitcode, "w"), "%d" % exitcode
 
 def debug():
     args = sys.argv[1:]
@@ -78,39 +116,14 @@ def debug():
         sys.exit(1)
 
     command = args
-    stdin = sys.stdin.read()
+    input = sys.stdin.read()
 
     tmpdir = os.environ.get("MAILPIPE_TMPDIR", "/tmp/mailpipe-debug")
-    digest = md5.md5(`command` + stdin).hexdigest()
-
+    digest = md5.md5(`command` + input).hexdigest()
     path = os.path.join(tmpdir, digest)
-    makedirs(path)
 
-    paths = DebugPaths(path)
-    
-    print >> file(paths.id, "w"), fmt_id()
-    print >> file(paths.env, "w").write(fmt_env())
-
-    if stdin:
-        file(paths.stdin, "w").write(stdin)
-
-    try:
-        os.symlink(sys.argv[0], paths.rerun)
-    except OSError:
-        pass
-
-    if command:
-        print >> file(paths.command, "w"), fmt_command(command)
-
-        stdout, stderr, exitcode = run(command, stdin)
-        if stdout:
-            file(paths.stdout, "w").write(stdout)
-
-        if stderr:
-            file(paths.stderr, "w").write(stderr)
-
-        if exitcode is not None:
-            print >> file(paths.exitcode, "w"), "%d" % exitcode
+    state = Context(path)
+    state.save(input, command)
 
 def rerun():
     args = sys.argv[1:]
